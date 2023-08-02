ff1dca35294b5433d63391d3aa7e6d40b87890f5
21Alul21 <augustine.alul@yahoo.com>
2023-08-02 15:15:24 +0000

    updated

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt*n/).join
-=======
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt*n/).join
->>>>>>> 1adad6a3b8a1733e1ac6d6d66e3a4463d7d6ef9a
+ARGV[0].scan(/hbt*n/).join
