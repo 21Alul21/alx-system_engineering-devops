d3fd7460364cd537627f499e6730fb90ed95298a
21Alul21 <augustine.alul@yahoo.com>
2023-09-26 14:12:15 -0700

    updates

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,6 @@
-#This manifest configures a web server using puppet to run nginx
+# Installs a Nginx server
 
-package {'nginx':
-  ensure => installed
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-file {'/var/www/html/index.html':
-  ensure => file,
-  content => 'Hello World!'
-}
-
